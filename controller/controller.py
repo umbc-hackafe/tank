bc097e7e41c49f082971a1de5f7a34495e879da4
Alexander Bauer <sasha@crofter.org>
2014-09-27 21:57:39 -0400

    Implement RPC mocking in controller

--- controller/controller.py
+++ controller/controller.py
@@ -2,6 +2,13 @@
 from __future__ import print_function
 import sys, time, argparse, threading
 
+mock = None
+
+try:
+    import mock
+except ImportError:
+    pass
+
 try:
     import xmlrpclib as xmlrpcclient
 except ImportError:
@@ -19,10 +26,18 @@ def parse(raw_args):
             default="http://localhost:1411/TANK")
     parser.add_argument("--continuous", "-c", action='store_true',
             help="retry the input driver if it fails")
+    if mock:
+        # Only allow mocking if the library is available.
+        parser.add_argument("--mock", action='store_true',
+                help="mock the RPC server")
+
     return parser.parse_args(raw_args)
 
 def main(args):
-    client = xmlrpcclient.ServerProxy(args.remote)
+    if "mock" in args and args.mock:
+        client = mock.MagicMock()
+    else:
+        client = xmlrpcclient.ServerProxy(args.remote)
 
     # Set up a thread to ping the RPC server constantly. If it drops for
     # more than 5 seconds, all of the motors will brake.
