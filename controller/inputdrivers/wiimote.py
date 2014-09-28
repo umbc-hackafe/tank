936cbf337298301406805e5347f079389a063f46
Alexander Bauer <sasha@crofter.org>
2014-09-27 23:17:22 -0400

    Fix up coding style a bit

--- controller/inputdrivers/wiimote.py
+++ controller/inputdrivers/wiimote.py
@@ -1,11 +1,12 @@
-from __future__ import division
+from __future__ import division, print_function
 import sys, time
 cwiid = None
 
 def imports():
     global cwiid
     try:
-        import cwiid
+        if not cwiid:
+            import cwiid
     except ImportError:
         print("Could not import cwiid. Is the package installed? Are you using python2?")
         sys.exit(1)
@@ -48,7 +49,8 @@ def main(client, args):
     last = None
 
     while True:
-        if (wm.state['buttons'] & cwiid.BTN_HOME) > 0:
+        buttons = wm.state['buttons']
+        if buttons & cwiid.BTN_HOME > 0:
             break
 
         # Print nunchuck values if appropriate.
@@ -56,12 +58,14 @@ def main(client, args):
             position = wm.state['nunchuk']['stick']
             x, y = position
             if abs(x - JOY_REST[0]) > JOY_THRESHOLD:
-                steer = max(min((x - JOY_REST[0]) / (JOY_MAX[0] - JOY_REST[0]), 1), -1)
+                steer = max(min((x - JOY_REST[0]) / (JOY_MAX[0] -
+                    JOY_REST[0]), 1), -1)
             else:
                 steer = 0
 
             if abs(y - JOY_REST[1]) > JOY_THRESHOLD:
-                speed = max(min((y - JOY_REST[1]) / (JOY_MAX[1] - JOY_REST[1]), 1), -1)
+                speed = max(min((y - JOY_REST[1]) / (JOY_MAX[1] -
+                    JOY_REST[1]), 1), -1)
             else:
                 speed = 0
 
